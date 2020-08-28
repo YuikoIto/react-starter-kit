b1ad0440461f93e77f7502b3b20af16ad4e6d4ee
YuikoIto <toffywing@gmail.com>
2020-08-28 23:47:24 +0900

    button

---
+++ src/Counter.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Counter = () => {
+  const value = 0;
+  return (
+    <div>
+      <div>value:{value}</div>
+      <button>+1</button>
+      <button>-1</button>
+    </div>
+  );
+};
+
+export default Counter;
